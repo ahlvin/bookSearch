d5efa5a6ba7699d4f578b3e53a6d80bc3e40b0ea
vineet ahluwalia <ahluwalia.vineet@gmail.com>
2019-03-04 21:59:35 -0500

    initial

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Classes from "./layout.module.css";
-import Aux from "../../hoc/Aux";
+
 const Layout = props => {
   return (
-    <Aux>
+    <div>
       <div className={Classes.header}>
         {/* <h2 style={{ margin: 0 }}>Book Finder</h2> */}
       </div>
       {props.children}
-    </Aux>
+    </div>
   );
 };
 
